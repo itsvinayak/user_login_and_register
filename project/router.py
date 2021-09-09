65556722b9d09e2aab948d63c0cf8519cf3707bc
Sanaf <40656876+0x30c4@users.noreply.github.com>
2021-09-10 02:40:59 +0600

    Syntax Issue

--- project/router.py
+++ project/router.py
@@ -2,4 +2,4 @@ from user.api.viewsets import userviewsets
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('user',userviewsets,base_name='user_api')
+router.register('user', userviewsets, base_name='user_api')
