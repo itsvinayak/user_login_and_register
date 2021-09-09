440f918a521ffe3b18723b11b04322986961887f
Sanaf <40656876+0x30c4@users.noreply.github.com>
2021-09-10 02:37:47 +0600

    Syntax Issue

--- user/urls.py
+++ user/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-         path('',views.index,name='index'),
+         path('', views.index, name='index'),
 ]
