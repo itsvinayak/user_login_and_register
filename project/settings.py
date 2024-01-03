5c348ea8d879c5c7cade911dc807468dd5036407
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2024-01-03 15:52:51 +0530

    logout fixed used django 4

--- project/settings.py
+++ project/settings.py
@@ -83,9 +83,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
                'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES':(
-                'rest_framework.permissions.IsAuthenticated',
-    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 
 }
 
