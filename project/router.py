57ccf69e953ecb7e33053892a42b0531f8bca350
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2024-01-17 19:50:10 +0530

    updated to django 4 and crispy-bootstrap5 (#18)
    changing Django version to 4

--- project/router.py
+++ project/router.py
@@ -2,4 +2,6 @@ from user.api.viewsets import userviewsets
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('user', userviewsets, base_name='user_api')
+router.register('user', userviewsets, basename='user_api')
+
+
